8226d76c58e7c2e50e43075cb7cec64ffa8b98e1
Saturn-98 <ishaan98jaiswal@gmail.com>
2024-11-04 13:20:38 +0530

    Removing Password Encoder

--- src/main/java/com/example/demo4/SecurityApp/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo4/SecurityApp/advice/GlobalExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.example.demo4.SecurityApp.advice;
 
 import com.example.demo4.SecurityApp.exceptions.ResourceNotFoundException;
-import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
